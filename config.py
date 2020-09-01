d61c0c3bf74c7fdcc55f1685bd548ea2ab8cbeba
marcorss <1078231846@qq.com>
2020-09-01 14:36:33 +0800

    new advGAN_tf2

--- config.py
+++ config.py
@@ -1,26 +1,26 @@
 from easydict import EasyDict as edict
 
 # class name list
-CLASS_NAME = ['art_exposed', 'baby_naked', 'child_naked', 'exposed', 'feet', 'intimate_nannan',
-              'intimate_nannv', 'intimate_nvnv', 'leggy', 'lutuitexie', 'midriff', 'mild_cleavage',
-              'moderate_cleavage', 'naked', 'nan_midriff', 'nan_zhichuanneiyineiku', 'nanxiong',
-              'normal', 'nv_zhichuanneiyineiku', 'porn', 'sex', 'sex_toy', 'sexy_qitaluolu',
-              'sexy_tunbu', 'sexy_xiatitexie', 'sexy_xiongbutexie', 'shield_breast', 'sm']
+CLASS_NAME = ['0', '1', '2', '3', '4', '5', '6',
+              '7', '8', '9', '10', '11', '12',
+              '13', '14', '15', '16', '17',
+              '18', '19', '20', '21', '22', '23',
+              '24', '25', '26', '27', '28']
 
 __C                     = edict()
 # Consumers can get config by: from config import cfg
 
 cfg                     = __C
 
-__C.TRAIN_ANNOT_PATH    = '/data/project/madanfeng/test/nn_robust_attacks/attack_tf2/data/dataset/sex_new_10000.list'  # 通过 train_annot_path 标签文档来读取训练图片
-__C.TEST_ANNOT_PATH     = '/data/project/madanfeng/test/nn_robust_attacks/attack_tf2/data/dataset/sex_test_3000.list'
+__C.TRAIN_ANNOT_PATH    = './data/train.txt'  # 通过 train_annot_path 标签文档来读取训练图片
+__C.TEST_ANNOT_PATH     = './data/test.txt'
 __C.SAVE_ADV_DIR        = './output/'     # 生成器生成对抗样本(包括扰动)保存的地址
 # __C.LOG_PATH            = './log/sex/log_01.txt'  # 日志保存地址
 
-__C.MODEL_PATH          = '/data/project/madanfeng/test/nn_robust_attacks/attack_tf2/model/model.stage3.03-0.8788.v2_320_tf2.hdf5'
+__C.MODEL_PATH          = './model/model.stage3.03-0.8788.v2_320_tf2.hdf5'
 __C.GEN_SAVE_DIR        = "./weights/generator/"       # 在训练时，用于保存生成器模型的文件夹
 __C.DISC_SAVE_DIR       = "./weights/discriminator/"   # 在训练时，用于保存判别器模型的文件夹
-__C.GENERATOR_PATH      = "./weights/generator/last.h5"  # 在测试时，训练好的生成器模型地址
+__C.GENERATOR_PATH      = "./weights/generator/1.h5"  # 在测试时，训练好的生成器模型地址
 
 
 __C.GPU_IDS             = '6'
